e4dff999d215cb5286fc29dc6b047e92d0fb74d6
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-23 00:49:27 +0530

    style to table

--- client/src/Component/userDetail.jsx
+++ client/src/Component/userDetail.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
-
+import "./styles/UserDetail.css"
 export const UserDetail = () => {
   return (
-    <div>userDetailpage</div>
+    <div>
+      <div className='filter_div'>
+          <select>
+            <option value="">Filter By Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+      </div>
+
+      <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>First</th>
+          <th>last</th>
+          <th>Email</th>
+          <th>Location</th>
+          <th>Pin</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Nitesh</td>
+            <td>Nitesh</td>
+            <td>Singh Rawat</td>
+            <td>nitesh@gmail.com</td>
+            <td>Delhi</td>
+            <td>110003</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
