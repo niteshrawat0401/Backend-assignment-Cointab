dad29aef6d02a86d9e4e36efd7c1833ea932c021
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-23 00:12:31 +0530

    add css in home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './Component/Home';
+import { userDetail } from './Component/userDetail';
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+    
     </div>
   );
 }
--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import "./styles/Home.css"
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
+  const navigate= useNavigate();
   return (
-    <div>Home</div>
+    <div className='outer_div'>
+        <button>Fetch Users</button>
+        <button>Delete Users</button>
+        <button onClick={()=>navigate("/user")}>User Details</button>
+    </div>
   )
 }
