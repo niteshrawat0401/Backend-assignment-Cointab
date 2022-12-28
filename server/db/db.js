9a70bea2d2f7328f31a3872959eb7831bdbec158
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-28 21:48:13 +0530

    add loop

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+mongoose.set('strictQuery', true);
 export default mongoose.connect(
   "mongodb+srv://nitesh:niteshcointab@cluster0.i6pwrx9.mongodb.net/cointab?retryWrites=true&w=majority"
 );
